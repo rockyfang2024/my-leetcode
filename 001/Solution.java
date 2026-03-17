13e5acc6fadc6a9bf151c0644142b1d4c359b112
rockyfang2024 <rockyfang2024@163.com>
2026-03-18 00:45:44 +0800

    Create Solution.java

---
+++ 001/Solution.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Finds two numbers in the array that add up to a specific target,
+ * and returns their indices.
+ * 
+ * @param nums   the input array of integers
+ * @param target the target sum we need to achieve
+ * @return an array containing the indices of the two numbers
+ * @throws IllegalArgumentException if no valid solution exists
+ */
+public int[] twoSum(int[] nums, int target) {
+    // Map to store number -> its corresponding index (optimization: no need for list storage)
+    Map<Integer, Integer> numToIndexMap = new HashMap<>();
+    
+    // One-pass traversal to complete both storage and lookup (no need for two separate loops)
+    for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        // If complement exists in the map, return the result immediately
+        if (numToIndexMap.containsKey(complement)) {
+            return new int[]{numToIndexMap.get(complement), i};
+        }
+        // If complement does not exist, store current number and its index
+        numToIndexMap.put(nums[i], i);
+    }
+    
+    // The problem guarantees a solution exists, this is just for syntax completeness
+    throw new IllegalArgumentException("No two sum solution");
+}
